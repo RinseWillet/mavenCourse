6d1f6209938d8948a081fca7154905f59735eccb
r-willet <rinse.willet@luminis.eu>
2026-06-02 13:37:51 +0200

    added vintage for junit4 and 5 test

---
+++ mb2g-testing/src/test/java/guru/springframework/JUNit5FauxTest.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JUNit5FauxTest {
+
+	@Test
+	void someTestforJUnit5(){
+		System.out.println("I Ran, I Saw, I Crashed...");
+	}
+
+}
