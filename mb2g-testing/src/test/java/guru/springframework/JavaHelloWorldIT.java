880f6b5e94327c02a6837b152bcac0e5e12319e6
r-willet <rinse.willet@luminis.eu>
2026-06-02 14:28:03 +0200

    added integration test

---
+++ mb2g-testing/src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+
+	@Test
+	void myFauxIntegrationTest() {
+
+		System.out.println("My IT Ran");
+	}
+}
